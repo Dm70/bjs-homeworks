d0c099e836a2dde64fd0f15f0a1ee1beaa10a4c1
Dmitriy Medvedev <walkin.ru@gmail.com>
2021-03-12 23:48:02 +0400

    Домашнее задание к занятию 1.2 «Расширенный синтаксис». Внесены изменения на основании предложений преподавателя

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,26 +1,23 @@
+'use strict'
 function getResult(a,b,c){
-    'use strict'
-	// код для задачи №1 писать здесь   //2x^2+4x-3=0, D = b² - 4ac
+    // код для задачи №1 писать здесь   //2x^2+4x-3=0, D = b² - 4ac
 	let D = Math.pow(b, 2) - 4 * a * c;
     let x = [];
 
     if (D < 0) {
-    	return x;
+    	// return x;
     } else if (D === 0) {
-    	x.push((-b + Math.sqrt(D)) / (2 * a));
-    	return x;
+    	x.push(-b / (2 * a));
     } else if (D > 0) {
 	    x.push((-b + Math.sqrt(D)) / (2 * a));
 	    x.push((-b - Math.sqrt(D)) / (2 * a));
-	    return x;
     }
 
-    // return x;
+    return x;
 }
 
 function getAverageMark(marks){
-	'use strict'
-    // код для задачи №2 писать здесь
+	// код для задачи №2 писать здесь
 	let sumMarks = 0;
 	let averageMark = 0;
 
@@ -40,17 +37,13 @@ function getAverageMark(marks){
 }
 
 function askDrink(name,dateOfBirthday){
-	'use strict'
-    // код для задачи №3 писать здесь
-	let myName = name;
-	let fullYears = dateOfBirthday.getFullYear();
-	let currentDate = new Date().getFullYear();
-	let result = currentDate - fullYears;
+	// код для задачи №3 писать здесь
+	// let fullYears = dateOfBirthday.getFullYear();
+	// let currentDate = new Date().getFullYear();
+	// let result = currentDate - fullYears;
+	let age = new Date().getFullYear() - dateOfBirthday.getFullYear();  // То же верно.Без объявления лишних
+																		// переменных (рекомендации преподавателя Владимира Чебукина)
 
-	return (result > 18) ? alert('Не желаете ли олд-фэшн ' + myName +'?') : alert('Сожалею, ' + myName + ', но' +
-		' я не' +
-		' могу' +
-		' вам' +
-		' продать алкоголь. Могу предложить вам замечательный клюквенный компот!');
+	return (age > 18) ? alert(`Не желаете ли олд-фэшн ${name}?`) : alert(`Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот`);
     // return result;
 }
